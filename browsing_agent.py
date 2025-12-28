3bb1cee932ec07d5cee7a93d47e9c0ff948ba2d7
Yasir <147266162+Blazikener@users.noreply.github.com>
2025-12-28 23:13:22 +0400

    Update browsing_agent.py

--- browsing_agent.py
+++ browsing_agent.py
@@ -1,14 +1,19 @@
 from browser_use import Agent, ChatOpenAI
 from dotenv import load_dotenv
 import asyncio
+from scrape import generate_test_cases, ExtractedWebsiteData, scrape_website, extract_website_intelligence
+import json
 
 load_dotenv()
 
 async def main():
-    llm = ChatOpenAI(model="gpt-4.1-mini")
-    task = "just open the browser and go to the website https://www.google.com, its a trail run"
-    agent = Agent(task=task, llm=llm)
-    await agent.run()
+    with open("tests.json", "r") as f:
+        tests = json.load(f)
+    for i in range(len(tests)):
+        task = tests[i]
+        llm = ChatOpenAI(model="gpt-4.1-mini")
+        agent = Agent(task=task, llm=llm)
+        await agent.run()
 
 if __name__ == "__main__":
     asyncio.run(main())
